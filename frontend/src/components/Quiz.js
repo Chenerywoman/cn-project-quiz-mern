57b34fc0471d3cc320078d40cb94b8d0d3c751e9
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-13 20:45:26 +0000

    getting data from quiz & rendering on page

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,27 +1,60 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-
+// import scrambleAnswers from  '../helpers';
 
 const Quiz = () => {
 
     const [questions, setQuestions] = useState([])
+    const [category, setCategory] = useState([])
 
+    const scrambleAnswers = (questions) => {
+
+        const questionandAnswers = questions.reduce((acc, curr) => {
+    
+            let answers = [{answer: curr.correct_answer, correct: true}, {answer: curr.incorrect_answers[0], correct:false}, {answer: curr.incorrect_answers[1], correct:false}, {answer: curr.incorrect_answers[2], correct:false}];
+            let mixedAnswers = [];
+            
+            let num = 4;
+            
+            for (let i = 0; i < 4; i++){
+    
+                let randInt = Math.floor(Math.random() * num);
+    
+                num--
+    
+                mixedAnswers.push(answers[randInt]);
+                answers.splice(randInt, 1)
+    
+            }
+    
+            acc.push({question: curr.question, answers: mixedAnswers});
+           return acc;
+    
+        }, [])
+    
+        return questionandAnswers;
+    
+    };
 
     const fetchQuestions = async () => {
 
-        const response = await axios.get('https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple');
+        try {
+            const response = await axios.get('https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple');
 
-        console.log('response')
-        console.log(response.data.results)
+            let receivedQuestions = [...response.data.results];
+            // console.log(receivedQuestions)
 
-        let receivedQuestions = [...response.data.results];
+            let questionsAndScrambledAnswers = scrambleAnswers(receivedQuestions);
+            console.log('questions and scrambled answers');
+            console.log(questionsAndScrambledAnswers);
+    
+            setQuestions(questionsAndScrambledAnswers);
+            setCategory(receivedQuestions[0].category);
 
-        setQuestions(receivedQuestions);
 
-        console.log('questions')
-        console.log(questions)
-
-
+        } catch (error) {
+            console.log(error)
+        }
 
     }
 
@@ -29,13 +62,28 @@ const Quiz = () => {
 
 
     return (
-        <div>
+      <div>
         <h1>Quiz Page</h1>
-        <h2>Category:{} </h2>
-        <form>
-
-        </form>
-            
+        <h2>Category:{category} </h2>
+            <form>
+                {questions.map((question, ind) => {
+                    return (
+                            <div key={ind} >
+                                <p> {question.question} </p>
+                                {question.answers.map((answer, ind) => {
+                                    return(
+                                        <div key={ind}>
+                                            <label htmlFor={ind}>{answer.answer}</label>
+                                            <input type="radio" name={ind} value={answer.correct} />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })
+                }
+                <input type="submit" value="Submit" />
+            </form>            
         </div>
     )
 }
