c53ed1376415df6179e4207e17574f8ee1228e9e
Kat Archer <katrinaaarcher@hotmail.co.uk>
2021-01-14 11:08:42 +0000

    backend score route done

--- server.js
+++ server.js
@@ -23,7 +23,31 @@ app.get('/', (req, res) => {
     res.send("Hello from Nodejs");
 });
 
+//quiz scores
+app.post('/quiz', (req, res) => {
+    console.log("reaching backend"); //checking data is received on backend
+    console.log(req.body);
 
+    // await Results.create({
+    //     score: req.body.score,
+    //     category: req.body.category,
+    //     difficulty: req.body.difficulty,
+    //     time: req.body.time,
+    //     id: req.body.id, //add later after User db created
+    // })
+
+    res.json({ //sending message to front-end
+        message: "User was registered"
+    });
+});
+
+
+
+
+//error handling
+app.get("*", (req, res) => {
+    res.send("error");
+});
 
 app.listen(5000, () => { 
     console.log('server is running on port 5000');
