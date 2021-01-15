7936148a13c7aec92d7afdc4aef0af03ae71670e
Kat Archer <katrinaaarcher@hotmail.co.uk>
2021-01-15 11:05:43 +0000

    league done for now

--- frontend/src/components/League.js
+++ frontend/src/components/League.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const League = () => {
+    const [user, setUser] = useState({});
+    const [result, setResult] = useState({});
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const fetchData = async () => {
+        const response = await axios.get('/league');
+        setUser(response.data.users);
+        setResult(response.data.results);
+
+    };
+
+
     return (
         <div>
             <h3>League Table</h3>
@@ -8,6 +24,19 @@ const League = () => {
             <div id="table-container" >
                 <tr>
                     <th>Position</th>
+                    <th>Score</th>
+                    <th>Time</th>
+                    <th>User</th>
+                    <th>Category</th>
+                    <th>Difficulty</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>{result.score}</td>
+                    <td>{result.time}</td>
+                    <td>{user.name}</td>
+                    <td>{result.category}</td>
+                    <td>{result.difficulty}</td>
                 </tr>
             </div>
         </div>
