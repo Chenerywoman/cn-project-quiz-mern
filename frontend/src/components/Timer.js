b7cfcebb41d31bdc87d2e74eab89ac5c17c2ebac
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-18 15:30:25 +0000

    adding first draft timer

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,36 +1,90 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
 
-class Timer extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {date: new Date()};
-    }
+const Timer = () => {
   
-    componentDidMount() {
-      this.timerID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   
-    componentWillUnmount() {
-      clearInterval(this.timerID);
-    }
-  
-    tick() {
-      this.setState({
-        date: new Date()
-      });
-    }
-  
-    render() {
-      return (
-        <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
+  const toggle = () => {
+    setIsActive(!isActive);
   }
 
-  export default Timer;
+  const reset = () => {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000)
+
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds])
+
+  return (
+    <>
+      <div>
+      {seconds}s
+      </div>
+      <button onClick={toggle}>
+        {isActive ? 'Pause' : 'Start'}
+      </button>
+      <button onClick={reset}>
+        Reset
+      </button>
+    </>
+  )
+}
+
+export default Timer
+
+
+
+
+
+
+
+// import React from 'react';
+
+// class Timer extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {date: new Date()};
+//     }
+  
+//     componentDidMount() {
+//       this.timerID = setInterval(
+//         () => this.tick(),
+//         1000
+//       );
+//     }
+  
+//     componentWillUnmount() {
+//       clearInterval(this.timerID);
+//     }
+  
+//     tick() {
+//       this.setState({
+//         date: new Date()
+//       });
+//     }
+  
+//     render() {
+//       return (
+//         <div>
+//           <h1>Hello, world!</h1>
+//           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//         </div>
+//       );
+//     }
+//   }
+
+//   export default Timer;
