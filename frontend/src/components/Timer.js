62ed86c386b620d1b485b9591d8a7e3d5f76cc00
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-18 16:21:18 +0000

    simplifying timer without buttons

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,90 +1,126 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 
 const Timer = () => {
   
   const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
   
-  const toggle = () => {
-    setIsActive(!isActive);
-  }
-
-  const reset = () => {
-    setSeconds(0);
-    setIsActive(false);
-  }
-
   useEffect(() => {
-    let interval = null;
-
-    if (isActive) {
-      interval = setInterval(() => {
+    let interval = setInterval(() => {
         setSeconds(seconds => seconds + 1);
       }, 1000)
 
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval)
-    }
-
     return () => clearInterval(interval);
-  }, [isActive, seconds])
+  }, [seconds])
 
+  console.log(seconds)
   return (
     <>
       <div>
       {seconds}s
       </div>
-      <button onClick={toggle}>
-        {isActive ? 'Pause' : 'Start'}
-      </button>
-      <button onClick={reset}>
-        Reset
-      </button>
     </>
   )
 }
 
 export default Timer
 
+// import React, {useEffect, useState} from 'react'
 
-
-
-
-
-
-// import React from 'react';
-
-// class Timer extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {date: new Date()};
-//     }
+// const Timer = () => {
   
-//     componentDidMount() {
-//       this.timerID = setInterval(
-//         () => this.tick(),
-//         1000
-//       );
-//     }
+//   const [seconds, setSeconds] = useState(0);
+//   const [isActive, setIsActive] = useState(true);
   
-//     componentWillUnmount() {
-//       clearInterval(this.timerID);
-//     }
-  
-//     tick() {
-//       this.setState({
-//         date: new Date()
-//       });
-//     }
-  
-//     render() {
-//       return (
-//         <div>
-//           <h1>Hello, world!</h1>
-//           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//         </div>
-//       );
-//     }
+//   const toggle = () => {
+//     setIsActive(!isActive);
 //   }
 
-//   export default Timer;
+//   const reset = () => {
+//     setSeconds(0);
+//     setIsActive(false);
+//   }
+
+//   useEffect(() => {
+//     let interval = null;
+
+//     if (isActive) {
+
+//       interval = setInterval(() => {
+//         setSeconds(seconds => seconds + 1);
+//       }, 1000)
+    
+//     } else if (!isActive && seconds !== 0) {
+//       clearInterval(interval)
+//     }
+
+//     return () => clearInterval(interval);
+//   }, [seconds])
+
+//   console.log(seconds)
+//   return (
+//     <>
+//       <div>
+//       {seconds}s
+//       </div>
+//       <button onClick={toggle}>
+//         {isActive ? 'Pause' : 'Start'}
+//       </button>
+//       <button onClick={reset}>
+//         Reset
+//       </button>
+//     </>
+//   )
+// }
+
+// export default Timer
+
+// import React, {useEffect, useState} from 'react'
+
+// const Timer = () => {
+  
+//   const [seconds, setSeconds] = useState('00');
+//   const [minutes, setMinutes] = useState('00');
+//   const [isActive, setIsActive] = useState(false);
+//   const [counter, setCounter] = useState(0);
+  
+//   const toggle = () => {
+//     setIsActive(!isActive);
+//   }
+
+//   const reset = () => {
+//     setSeconds(0);
+//     setIsActive(false);
+//   }
+
+//   useEffect(() => {
+//     let interval = null;
+
+//     if (isActive) {
+
+//       interval = setInterval(() => {
+//         setSeconds(seconds => seconds + 1);
+//       }, 1000)
+
+//     } else if (!isActive && seconds !== 0) {
+//       clearInterval(interval)
+//     }
+
+//     return () => clearInterval(interval);
+//   }, [isActive, seconds])
+
+//   return (
+//     <>
+//       <div>
+//       {seconds}s
+//       </div>
+//       <button onClick={toggle}>
+//         {isActive ? 'Pause' : 'Start'}
+//       </button>
+//       <button onClick={reset}>
+//         Reset
+//       </button>
+//     </>
+//   )
+// }
+
+// export default Timer
