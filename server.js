bf903a2337680cf11698203c281491fdff50057a
Kat Archer <katrinaaarcher@hotmail.co.uk>
2021-01-25 11:44:32 +0000

    heroku 10

--- server.js
+++ server.js
@@ -352,7 +352,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
 
-app.listen(5000, () => { 
-    console.log('server is running on port 5000');
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+  
+console.log(`Server listening on ${port}`);
 
